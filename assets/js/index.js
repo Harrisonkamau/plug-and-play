e154f9acc8a9794624ad28ce2971130bc8ab2aa6
Harrisonkamau <kamauharrison87@gmail.com>
2016-07-07 19:38:33 +0300

    implement button animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,4 +12,13 @@ $(document).ready(function(){
         $('html, body').animate({scrollBottom : 0},800);
         return false;
     });
+    // JS for highlight selected button
+      $(function() {
+          $("a").click(function() {
+            // remove classes from all
+            $("a").removeClass("active");
+            // add class to the one we clicked
+            $(this).addClass("active");
+         });
+      });
 });
